18c2c63ad3900e067f7ae091ff14117f003fcf8d
Mark Antonenko <2002mark.a@gmail.com>
2021-08-28 18:24:13 +0300

    28.08.2021
    trying to create a telegram bot

--- index.js
+++ index.js
@@ -4,13 +4,26 @@
 
 // https://api.telegram.org/bot1963725285:AAGkr_0BXtcu48NbkwB2v0Duh6neuSWM38w/getUpdates
 
-document.querySelector('button').onclick = function() {
-    let messaage = document.querySelector('.message').value;
-    let token = '1963725285:AAGkr_0BXtcu48NbkwB2v0Duh6neuSWM38w';
-    let chat_id = "695680789";
-    const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=`;
-    let xhttp = new XMLHttpRequest();
-    xhttp.open("GET", url + message, true);
-    xhttp.send();
+// document.querySelector('button').click = function() {
+//     let messaage = document.querySelector('.message').value;
+//     const token = '1963725285:AAGkr_0BXtcu48NbkwB2v0Duh6neuSWM38w';
+//     // let chat_id = "695680789";
+//     let url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=695680789&text=`;
+//     let xhttp = new XMLHttpRequest();
+//     xhttp.open("GET", url+message, true);
+//     xhttp.send();
 
-}
+// }
+
+const btnAdd = document.querySelector('button');
+
+btnAdd.addEventListener('click', function(){
+    //     let messaage = document.querySelector('.message').value;
+    // const token = '1963725285:AAGkr_0BXtcu48NbkwB2v0Duh6neuSWM38w';
+    // // let chat_id = "695680789";
+    // let url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=695680789&text=`;
+    // let xhttp = new XMLHttpRequest();
+    // xhttp.open("GET", url+message, true);
+    // xhttp.send();
+    console.log(`hthh`);
+})
