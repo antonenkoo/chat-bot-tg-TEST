cb9e7a202664a08542e3f375b0bcd263787dc832
Mark Antonenko <2002mark.a@gmail.com>
2021-08-30 13:12:54 +0300

    add test word

--- js/task-08.js
+++ js/task-08.js
@@ -4,22 +4,50 @@ document.forms.publish.onsubmit = function() {
   let chat_id = "695680789";
   let url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=`;
   let xhttp = new XMLHttpRequest();
-  xhttp.open("GET", url + message, true);
-  xhttp.send();
+  if (message == 'test') {
+        xhttp.open("GET", url + "вы ввели проверочное слово", true);
+        xhttp.send();
+    
+      } else {
+        xhttp.open("GET", url + message, true);
+        xhttp.send();
   
-  
+      }
   return false;
 };
 
+let zhttp = new XMLHttpRequest();
+url = `https://api.telegram.org/bot${token}/getUpdates`
+zhttp.open('GET', url, false)
+zhttp.send();
 
-// document.forms.publish.onsubmit = function eventForm(value) {
-//   if (value == 'test') {
-//     xhttp.open("GET", url + "вы ввели правельное слово", true);
-//     xhttp.send();
 
+
+
+// {"update_id":381278855,
+// "message":{
+//   "message_id":118,
+//   "from":{
+//       "id":695680789,
+//       "is_bot":false,
+//       "first_name":"moraks",
+//       "username":"morakss",
+//       "language_code":"ru"
+//   },
+//   "chat":{
+//       "id":695680789,
+//       "first_name":"moraks",
+//       "username":"morakss",
+//       "type":"private"
+//   },
+//     "date":1630314954,
+//     "text":"asd"
 //   }
 // }
 
+
+
+// https://api.telegram.org/bot1963725285:AAGkr_0BXtcu48NbkwB2v0Duh6neuSWM38w/getUpdates
 
 
 
